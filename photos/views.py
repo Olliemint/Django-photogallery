413ad040fe36c24f9db1f10624c00bae5d5bf740
Olliemint <furywrld5@gmail.com>
2022-05-27 00:04:30 +0300

    fixed SECRET_KEY bug

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from .models import Category,Photo
 
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+    if category is None:
+        photos = Photo.objects.all()
+        
+    else:
+        photos = Photo.objects.filter(category__name__contains = category)
+    
     categories = Category.objects.all()
     
-    photos = Photo.objects.all()
     context = {'categories': categories,'photos': photos}
     
     
@@ -23,5 +30,34 @@ def viewPhoto(request,pk):
 
 
 def addPhoto(request):
+    categories = Category.objects.all()
     
-    return render(request, 'photos/add.html')
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        
+        
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        
+        else:
+            category = None
+        
+        
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image
+        )
+    
+        return redirect('gallery')
+            
+        
+        
+    
+    context = {'categories': categories}
+    
+    
+    return render(request, 'photos/add.html',context)
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y4#cyip(uppwxeudhz6a*npo_j_sl_pf^=$3isg56!m+xa)8%w'
+SECRET_KEY = config("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-y4#cyip(uppwxeudhz6a*npo_j_sl_pf^=$3isg56!m+xa)8%w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
