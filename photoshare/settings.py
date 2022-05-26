fcde70b6962c4c18ce530910ffe73eeb0e9e9201
Olliemint <furywrld5@gmail.com>
2022-05-27 01:02:11 +0300

    fix deployment bug

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cornygallery.herokuapp.com']
+ALLOWED_HOSTS = ['cornygallery.herokuapp.com','127.0.0.1']
 
 
 # Application definition
